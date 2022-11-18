487aa6d68c3280ad6feae71a1d0a951c8879e257
jblessingHC <85644461+jblessingHC@users.noreply.github.com>
2022-11-18 09:47:33 -0500

    update to 0.3.0 Provider

--- pipeline3/main.tf
+++ pipeline3/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azuredevops = {
       source  = "microsoft/azuredevops"
-      version = ">=0.1.0"
+      version = ">=0.3.0"
     }
     azurerm = {
       source = "hashicorp/azurerm"
@@ -23,7 +23,7 @@ variable "branch_name" {}
 
 resource "azuredevops_build_definition" "build3" {
   project_id = var.project_id
-  name       = "03 - Push Inventory to Cloud Store"
+  name       = "03 - Store the State of TF CLI to AZ Cloud Store"
   path       = "\\pipelines"
 
   ci_trigger {
